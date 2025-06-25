54274d0764738002dcf30f95cf10d8ae59ec0099
Amel252 <amelghanem25@gmail.com>
2025-06-25 19:56:49 +0200

    Connexion avec mongo reussie

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-// on importe Express
+// on importe Express et mongoose
 const express = require("express");
-
+const mongoose = require("mongoose");
 // Tu charges les variables d’environnement depuis le fichier .env situé dans le dossier config/.
-require('dotenv').config({ path: './config/.env' })
-
+require("dotenv").config({ path: "./config/.env" })
 // Tu crées une instance de l’application Express , C’est sur cette instance (app) que tu vas :définir des routes (app.get(...), app.post(...)), ajouter des middlewares (app.use(...)) , connecter à une base de données, etc.
 const app = express();
 
+// le lien de mongoose avec le bdd atlas
+mongoose
+  .connect(process.env.MONGO)
+
+  .then(() => console.log("✅ Connexion à MongoDB Atlas réussie !"))
+  .catch((err) => {
+    console.error("❌ Erreur de connexion à MongoDB :", err.message);
+    process.exit(1); // Stoppe le serveur si la connexion échoue
+  });
 //Tu dis à Express de démarrer le serveur sur le port 5000.
 app.listen(process.env.PORT, () => {
     console.log(`listing on port ${process.env.PORT}`);
