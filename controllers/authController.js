c41cac7cb25951fe46093e9b820ad9f3aa4f7ac5
Amel252 <amelghanem25@gmail.com>
2025-06-29 15:06:45 +0200

    Faire la function d'Inscription (register)+ logIn et logout (connexion et déconnexion )

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,15 @@
 const UserModel = require("../models/user.model");
+const jwt = require("jsonwebtoken");
 
-//
+const maxAge = 3 * 24 * 60 * 60 * 1000;
+const createToken = (id) => {
+    return jwt.sign({ id }, process.env.TOKEN_SECRET, {
+        // notre token va expiré dans 3jrs (24*60*60*1000 = 24h)
+        expiresIn: maxAge,
+    });
+};
+
+// function signUp (inscription)
 module.exports.signUp = async (req, res) => {
     const { pseudo, email, password } = req.body;
     // body-parser : un middleware pour Express qui permet de lire et traiter les données envoyées dans le corps des requêtes HTTP
@@ -16,4 +25,30 @@ module.exports.signUp = async (req, res) => {
         console.error(error); // Pour voir l'erreur dans la console serveur
         res.status(500).json({ error: error.message });
     }
+};
+
+// function signIn (connexion ):
+module.exports.signIn = async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password)
+        return res.status(400).json({ message: "Champs requis manquants." });
+
+    try {
+        const user = await UserModel.login(email, password); // suppose que ta méthode login existe
+        const token = createToken(user._id);
+        // on crée un cookie qui s'appelle jtw
+        res.cookie("jwt", token, { httpOnly: true, maxAge });
+        res.status(200).json({
+            message: "Utilisateur connecté",
+            user: user._id,
+        });
+    } catch (error) {
+        res.status(401).json({ message: error.message });
+    }
+};
+module.exports.logout = (req, res) => {
+    // Tu remplaces le cookie jwt par une chaîne vide, et tu le fais expirer immédiatement (dans 1 milliseconde).
+    res.cookie("jwt", "", { maxAge: 1 });
+    res.status(200).json({ message: "Déconnexion réussie" });
+    res.redirect("/");
 };
--- models/user.model.js
+++ models/user.model.js
@@ -48,10 +48,33 @@ const userSchema = new mongoose.Schema(
 );
 // play function before save , hasher mon Mdp
 userSchema.pre("save", async function (next) {
+    // Cela permet de hasher le mot de passe uniquement s’il a été modifié.
+    if (!this.isModified("password")) return next();
     const salt = await bcrypt.genSalt();
     this.password = await bcrypt.hash(this.password, salt);
     next();
 });
+
+//quand on veux se connecter on doit désalir notre password
+userSchema.statics.login = async function (email, password) {
+    // cherche dans la base de données un utilisateur dont le champ email correspond à celui fourni.// Si un utilisateur est trouvé, il est stocké dans la variable user.
+    const user = await this.findOne({ email });
+    // Si l’utilisateur existe (non nul), on continue pour vérifier le mot de passe.
+    if (user) {
+        // compare va comparer le mail dans BD haché stocké  et le password fourni
+        const auth = await bcrypt.compare(password, user.password);
+
+        if (auth) {
+            // Si la comparaison est réussie (le mot de passe est correct), on retourne l’objet user.
+            return user;
+        }
+        // Sinon, on renvoie une erreur personnalisée
+        throw Error("password incorrecte ");
+    }
+    // Si aucun utilisateur n'est trouvé avec l’email donné, une erreur personnalisée est renvoyée : "email incorrect".
+    throw Error("email incorreste");
+};
+
 const UserModel = mongoose.model("user", userSchema);
 
 module.exports = UserModel;
