2ce565a768b9e73953b2fa8f8f726adb72b70d18
Amel252 <amelghanem25@gmail.com>
2025-07-22 20:18:18 +0200

    Rajout des Style déja prédefinis + Création de pages(home,profil, trending) + faire les Routes

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,5 +1,12 @@
-function App() {
-    return <></>;
-}
+import React from "react";
+import Routes from "./components/Routes/index.jsx";
+
+const App = () => {
+    return (
+        <div>
+            <Routes />
+        </div>
+    );
+};
 
 export default App;
---
+++ front-end/src/components/Routes/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../../pages/Home.jsx";
+import Trending from "../../pages/Trending.jsx";
+import Profile from "../../pages/Profil.jsx";
+
+const index = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/trending" element={<Trending />} />
+                <Route path="/profil" element={<Profile />} />
+            </Routes>
+        </BrowserRouter>
+    );
+};
+
+export default index;
