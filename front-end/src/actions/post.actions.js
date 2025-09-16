2ba5458cbd825f6d8a96ace06df2e99b461c56ae
Amel252 <amelghanem25@gmail.com>
2025-09-17 01:09:20 +0200

    Création de postAction & postReducer (pour like et unlike  post ),

--- front-end/src/actions/post.actions.js
+++ front-end/src/actions/post.actions.js
@@ -2,6 +2,9 @@ import axios from "axios";
 
 // posts
 export const GET_POSTS = "GET_POSTS";
+export const LIKE_POST = "LIKE_POST";
+export const UNLIKE_POST = "UNLIKE_POST";
+// Récupération des posts
 export const getPosts = () => {
     return (dispatch) => {
         return axios
@@ -11,6 +14,42 @@ export const getPosts = () => {
             })
             .catch((err) => {
                 console.log(err);
+            });
+    };
+};
+// Like d’un post
+export const likePost = (postId, userId) => {
+    return (dispatch) => {
+        return axios({
+            method: "patch",
+            url: `${import.meta.env.VITE_API_URI}/api/post/like-post/${postId}`,
+            data: { id: userId },
+        })
+            .then((res) => {
+                dispatch({ type: LIKE_POST, payload: { postId, userId } });
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+};
+// Unlike d’un post
+export const unlikePost = (postId, userId) => {
+    return (dispatch) => {
+        return axios
+            .patch(
+                `${
+                    import.meta.env.VITE_API_URI
+                }/api/post/unlike-post/${postId}`,
+                {
+                    id: userId,
+                }
+            )
+            .then(() => {
+                dispatch({ type: UNLIKE_POST, payload: { postId, userId } });
+            })
+            .catch((err) => {
+                console.error("Erreur lors du unlike :", err);
             });
     };
 };
--- front-end/src/redux/post.reducer.js
+++ front-end/src/redux/post.reducer.js
@@ -1,10 +1,39 @@
-import { GET_POSTS } from "../actions/post.actions";
+import { GET_POSTS, LIKE_POST, UNLIKE_POST } from "../actions/post.actions";
 
-const initialState = {};
-export default function postReducer(state = initialState, action) {
+const initialState = [];
+
+export default function postsReducer(state = initialState, action) {
     switch (action.type) {
+        // Récupère tous les posts
         case GET_POSTS:
             return action.payload;
+
+        // Like d’un post
+        case LIKE_POST:
+            return state.map((post) => {
+                if (post._id === action.payload.postId) {
+                    return {
+                        ...post,
+                        likers: [...post.likers, action.payload.userId],
+                    };
+                }
+                return post;
+            });
+
+        // Unlike d’un post
+        case UNLIKE_POST:
+            return state.map((post) => {
+                if (post._id === action.payload.postId) {
+                    return {
+                        ...post,
+                        likers: post.likers.filter(
+                            (id) => id !== action.payload.userId
+                        ),
+                    };
+                }
+                return post;
+            });
+
         default:
             return state;
     }
